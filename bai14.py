80e16bdeb3f200b0f5f0e8637b5603b06ead0645
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 13:54:34 +0700

    Update bai14.py

--- bai14.py
+++ bai14.py
@@ -1,25 +1,32 @@
 import math
 def check_snt(n):
-    a =0
-    for i in range(1,n+1):
-        if(n%i==0):
-            a =a+1
-    if(a==2):
+    if n<=1:
+        return False
+    elif n<=3:
         return True
-    return False
-def reverse(n):
-    list = []
-    while(n>0):
-        list.append(n%10)
-        n = n // 10
-    num = 0
-    for i in range(len(list),0,-1):
-        num = num + list[len(list)-i]*pow(10,i-1)
-    return num
+    elif n%2==0 or n%3==0:
+        return False
+    i = 5
+    while i<=math.sqrt(n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i = i+6
+    return True
+
+def dao(n):
+    l = []
+    while(n!=0):
+        l.append(n%10)
+        n =n//10
+    a = 0
+    for i in range(0,len(l)):
+        a = a + l[i]*pow(10,len(l)-1-i)
+    return a
+
 def find():
     for i in range(100,1000):
         if(check_snt(i)==True):
-            for j in range(0,reverse(i)):
-                if(pow(j,3)==reverse(i)):
-                    print(i)
+            if(math.ceil(pow(dao(i),1/3))==math.floor(pow(dao(i),1/3))):
+                print(i)
+
 find()
