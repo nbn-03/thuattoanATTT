26efdbe603584aa6f0893997963c8b95f6d476b8
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 14:04:19 +0700

    Update bai24.py

--- bai24.py
+++ bai24.py
@@ -1,29 +1,29 @@
 import math
 def check_snt(n):
-    a =0
-    for i in range(1,n+1):
-        if(n%i==0):
-            a =a+1
-    if(a==2):
+    if n<=1:
+        return False
+    elif n<=3:
         return True
-    return False
-def find_array(b):
-    list = []
-    for i in range (0,int(math.sqrt(b))+1):
-        list.append(pow(i,2))
-    return list
+    elif n%2==0 or n%3==0:
+        return False
+    i = 5
+    while i<=math.sqrt(n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i = i+6
+    return True
+def tao_mang(n):
+    return [i**2 for i in range(0,n)]
 def find(a,b):
-    #dem = 0
-    s1 = find_array(b)
-    s2 = find_array(b)
+    s1 = tao_mang(b)
+    s2 = tao_mang(b)
     for i in range(a,b+1):
         if(check_snt(i)==True):
-            for j in range(0,len(s1)):
-                for k in range(j,len(s2)):
-                    if(s1[j]+s2[k]==i):
-                        #dem = dem +1
-                        print(i)
-    #print(dem)
+            for k in range(0,len(s1)):
+                for l in range(k,len(s2)):
+                    if(s1[k]+s2[l]==i):
+                        print(i) 
+
 a = int(input("nhap so a: "))
 b = int(input("nhap so b: "))
 find(a,b)
