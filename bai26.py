3bda00642569da2343d44683876c0640dbf8ffd9
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 14:05:38 +0700

    Update bai26.py

--- bai26.py
+++ bai26.py
@@ -1,17 +1,23 @@
 import math
 def check_snt(n):
-    a =0
-    for i in range(1,n+1):
-        if(n%i==0):
-            a =a+1
-    if(a==2):
+    if n<=1:
+        return False
+    elif n<=3:
         return True
-    return False
+    elif n%2==0 or n%3==0:
+        return False
+    i = 5
+    while i<=math.sqrt(n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i = i+6
+    return True
 def find(n):
     for i in range(0,n):
-        for j in range(0,i+1):
-            if(check_snt(j) is True):
+        for j in range(0,i):
+            if check_snt(j) ==True:
                 if(i%j==0 and i%pow(j,2)==0):
-                    print(i,j)
+                    print(i)
+
 n = int(input("nhap so n: "))
 find(n)
