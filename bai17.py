0c7f3333c7509635af5301384501d447a838af79
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 13:56:21 +0700

    Update bai17.py

--- bai17.py
+++ bai17.py
@@ -1,12 +1,17 @@
 import math
 def check_snt(n):
-    a =0
-    for i in range(1,n+1):
-        if(n%i==0):
-            a =a+1
-    if(a==2):
+    if n<=1:
+        return False
+    elif n<=3:
         return True
-    return False
+    elif n%2==0 or n%3==0:
+        return False
+    i = 5
+    while i<=math.sqrt(n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i = i+6
+    return True
 def find(a,b,c):
     x = 1
     sum = a+b+c
