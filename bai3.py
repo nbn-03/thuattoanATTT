1f5e01bf0169cb14f3c22e1c9b595f4430b7e7cc
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 13:43:41 +0700

    Update bai3.py

--- bai3.py
+++ bai3.py
@@ -1,26 +1,29 @@
 import math
-def ktsnt(n):
-    a =0
-    for i in range(1,n+1):
-        if(n%i==0):
-            a =a+1
-    if(a==2):
+def check_snt(n):
+    if n<=1:
+        return False
+    elif n <=3:
         return True
-    return False
-def tinh(n):
-    list = []
-    p = 0
+    elif n%2==0 or n%3==0:
+        return False
+    i = 5 
+    while i <= math.sqrt(n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i = i +6
+    return True
+def uoc(n):
+    l = []
     for i in range(1,n+1):
-        if(n%i==0):
-            list.append(i)
-            p = p+i
-    s = len(list)
-    k = 0
+        if n%i ==0:
+            l.append(i)
+    k =0
     q = 0
-    for i in range(0,s):
-        if(ktsnt(list[i])==True):
-            k = k+1
-            q = q +list[i]
-    print(n+s-k-q+p)
-n = int(input('Nhap vao so n: '))
-tinh(n)
+    for i in l:
+        if check_snt(i) ==True:
+            k = k + 1
+            q = q + i
+    key = n + len(l) + sum(l) - k - q
+    return key
+n = int(input("nhap n: "))
+print(uoc(n))
