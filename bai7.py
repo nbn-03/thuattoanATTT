326f8ec310f3d10c00caa5f7cad4065fe623b75c
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 13:47:36 +0700

    Update bai7.py

--- bai7.py
+++ bai7.py
@@ -1,24 +1,33 @@
 import math
-def ktsnt(n):
-    a =0
-    for i in range(1,n+1):
-        if(n%i==0):
-            a =a+1
-    if(a==2):
+def check_snt(n):
+    if n<=1:
+        return False
+    elif n<=3:
         return True
-    return False
-def reverse(n):
-    list = []
-    while(n>0):
-        list.append(n%10)
-        n = n // 10
-    num = 0
-    for i in range(len(list),0,-1):
-        num = num + list[len(list)-i]*pow(10,i-1)
-    return num
+    elif n%2==0 or n%3==0:
+        return False
+    i = 5 
+    while i<=math.sqrt(n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i = i+6
+    return True
+
+def dao(n):
+    l =[]
+    while(n!=0):
+        l.append(n%10)
+        n = n//10
+    a = 0
+    for i in range(0,len(l)):
+        a = a + l[i] * pow(10,len(l)-1-i)
+    return a
+
 def find(n):
-    for i in range (2,n):
-        if((ktsnt(i)==True) and (ktsnt(reverse(i))==True)):
-            print(i)
-n = int(input('Nhap vao so n: '))
-find(n)
+    l = []
+    for i in range(1,n):
+        if(check_snt(i)==True and check_snt(dao(i))==True):
+            l.append(i)
+    return l
+n = int(input("nhap n: "))
+print(find(n))
