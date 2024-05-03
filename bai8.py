d5bc579b0a54ef6351ade5e1ab3210d6a8acf32e
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 13:52:40 +0700

    Update bai8.py

--- bai8.py
+++ bai8.py
@@ -1,15 +1,14 @@
-import math
-def t_prime(n):
-    list_t_prime = []
-    for i in range(1,n+1):     
-        count = 2
-        for j in range (2,i):
-            if (i % j == 0):
-                count += 1
-        if count == 3:
-            list_t_prime.append(i)       
-    print(list_t_prime)
-def main():
-    n = int(input('Nhap vao so n: '))
-    t_prime(n)   
-main()
+def uoc(n):
+    dem = 0
+    for i in range(2,n):
+        if(n%i==0):
+            dem = dem +1
+    return dem
+def find(n):
+    l = []
+    for i in range(1,n):
+        if(uoc(i)==1):
+            l.append(i)
+    return l
+n = int(input("nhap n: "))
+print(find(n))
