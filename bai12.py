4dee5c769993c40a1a3b52ec7bf720c27296751d
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 13:53:31 +0700

    Update bai12.py

--- bai12.py
+++ bai12.py
@@ -1,20 +1,26 @@
 import math
 def check_snt(n):
-    a =0
-    for i in range(1,n+1):
-        if(n%i==0):
-            a =a+1
-    if(a==2):
+    if n<=1:
+        return False
+    elif n<=3:
         return True
-    return False
+    elif n%2==0 or n%3==0:
+        return False
+    i = 5
+    while i<=math.sqrt(n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i = i+6
+    return True
+
 def find(n):
-    list1 = []
-    for i in range(0,n):
+    l = []
+    for i in range(1,n):
         if(check_snt(i)==True):
-            list1.append(i)
-    for i in range(0,len(list1)-3):
-        a = list1[i] + list1[i+1] + list1[i+2] + list1[i+3]
-        if(a<=n):
-            print(list1[i], list1[i+1], list1[i+2], list1[i+3])
-n = int(input("Nhap vao so n: "))
+            l.append(i)
+    for i in range(0,len(l)-3):
+        a = l[i]+l[i+1]+l[i+2]+l[i+3]
+        if(check_snt(a)==True and a<=n):
+            print(f"{l[i]}-{l[i+1]}-{l[i+2]}-{l[i+3]}")
+n = int(input("nhap n: "))
 find(n)
