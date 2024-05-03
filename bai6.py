6ab53df9a474ac5ae0381c1b8aeb3599654def49
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-05-03 13:46:00 +0700

    Update bai6.py

--- bai6.py
+++ bai6.py
@@ -1,22 +1,15 @@
-import math
-import time
-a = time.time()
 def uoc(n):
-    a = 0
-    for i in range (1,n):
-        if(n%i==0):
-           a = a + i
-    return a
-def kt(n):
-    list = []
-    for i in range (2,n):
-        a = uoc(i)
-        if(a>i):
-            b = uoc(a)
-            if(b==i):
-               list.append(i)
-               list.append(a) 
-    print(list)
-n = int(input('Nhap vao so n: '))
-kt(n)
-print(time.time()-a)
+    p = 0
+    for i in range(1,n):
+        if n%i==0:
+            p = p+i
+    return p
+def find(n):
+    for i in range(1,n+1):
+        p = uoc(i)
+        if(p>i):
+            a = uoc(p)
+            if(a==i):
+                print(f"{i}-{p}")
+n = int(input("nhap n: "))
+find(n)
